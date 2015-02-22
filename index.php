b8d0e75fb35f15ca2bb7f8f8abc46561ffc670e1
VoidWalker <sohanoleksa@gmail.com>
2015-02-22 20:44:07 +0200

    Check updation on file

--- index.php
+++ index.php
@@ -1,29 +1,67 @@
 <?php
-echo "Controll check!";
-$_updatedProductsPath = "cd_catalog.xml";
-$configurableProducts = array();
-if ($_updatedProductsPath !== false) {
+echo "Check!";
+$_fullFileProductsPath = "catalog.xml";
+$_updatedFileProductsPath = "updated.xml";
+$fullFileConfigurableProducts = array();
+$updatedFileConfigurableProducts = array();
+
+if($_updatedFileProductsPath !== false) {
     $domUpdated = new DOMDocument();
-    $stringFromFile = file_get_contents($_updatedProductsPath);
-    $domUpdated->loadXML($stringFromFile);
-    $xpathUpdated = new DOMXpath($domUpdated);
-    $products = $xpathUpdated->query('//product');
-    foreach ($products as $product) {
+    $stringFromUpdatedFile = file_get_contents($_updatedFileProductsPath);
+    $domUpdated->loadXML($stringFromUpdatedFile);
+    $xpathUpdated = new DOMXPath($domUpdated);
+    $productsUpdated = $xpathUpdated->query('//product');
+    foreach ($productsUpdated as $product) {
         if ($mainArticle = _getNodeValue($xpathUpdated, 'main_article', $product)) {
             $productType = _getNodeValue($xpathUpdated, 'product_type', $product);
             $sku = _getNodeValue($xpathUpdated, 'sku', $product);
             if ($productType == 'M') {
-                $configurableProducts[$mainArticle]['configurable'][$sku] = 1;
+                $updatedFileConfigurableProducts[$mainArticle]['configurable'][$sku] = 0;
             } elseif ($productType == 'A') {
-                $configurableProducts[$mainArticle]['simple'][$sku] = 1;
+                $updatedFileConfigurableProducts[$mainArticle]['simple'][$sku] = 0;
+            }
+        }
+    }
+
+}
+
+echo '<pre>updated';
+print_r($updatedFileConfigurableProducts);
+echo '</pre>';
+
+if ($_fullFileProductsPath !== false) {
+    $domFull = new DOMDocument();
+    $stringFromFile = file_get_contents($_fullFileProductsPath);
+    $domFull->loadXML($stringFromFile);
+    $xpathFull = new DOMXpath($domFull);
+    $productsFull = $xpathFull->query('//product');
+    foreach ($productsFull as $product) {
+        if ($mainArticle = _getNodeValue($xpathFull, 'main_article', $product)) {
+            $productType = _getNodeValue($xpathFull, 'product_type', $product);
+            $sku = _getNodeValue($xpathFull, 'sku', $product);
+            if ($productType == 'M') {
+                $fullFileConfigurableProducts[$mainArticle]['configurable'][$sku] = isset($updatedFileConfigurableProducts[$mainArticle]['configurable'][$sku]) ? 1 : 0;
+            } elseif ($productType == 'A') {
+                $fullFileConfigurableProducts[$mainArticle]['simple'][$sku] = isset($updatedFileConfigurableProducts[$mainArticle]['simple'][$sku]) ? 1 : 0;
             }
         }
     }
 }
-echo '<pre>';
-print_r($configurableProducts);
+
+echo '<pre>full';
+print_r($fullFileConfigurableProducts);
 echo '</pre>';
-//unset($domUpdated);
+
+
+/*
+function _checkUpdated($mainArticle, $sku)
+{
+    foreach ($updatedFileConfigurableProducts[$mainArticle][] as $) {
+
+    }
+
+}
+*/
 
 function _getNodeValue(DOMXPath $xpath, $query, $parentNode = null, $key = 0)
 {
